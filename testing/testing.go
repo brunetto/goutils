85cf59a6f00f1661681212db78c2dad0a21b6ef3
Brunetto Ziosi <brunetto.ziosi@pixartprinting.com>
2017-02-28 16:23:10 +0100

    tdded NotOk to testing

--- testing/testing.go
+++ testing/testing.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-// assert fails the test if the condition is false.
+// Assert fails the test if the condition is false.
 func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
@@ -19,7 +19,7 @@ func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	}
 }
 
-// ok fails the test if an err is not nil.
+// Ok fails the test if an err is not nil.
 func Ok(tb testing.TB, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
@@ -28,7 +28,16 @@ func Ok(tb testing.TB, err error) {
 	}
 }
 
-// equals fails the test if exp is not equal to act.
+// NotOk fails the test if an err is  nil.
+func NotOk(tb testing.TB, err error) {
+	if err == nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: expected error, not found: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// Equals fails the test if exp is not equal to act.
 func Equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
